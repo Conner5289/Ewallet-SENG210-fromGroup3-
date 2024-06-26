daa2920a795303fad1bc3e7bd87251bb695edcc0
opuebla56 <pueblao@ferris.edu>
2024-06-25 22:29:41 -0400

    Fulfilled user stories that deal with loading Income and Expense Files

--- src/main/java/org/ewallet/ExpenseCalculator.java
+++ src/main/java/org/ewallet/ExpenseCalculator.java
@@ -3,7 +3,10 @@ package org.ewallet;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.PrintWriter;
 
 public class ExpenseCalculator implements IExpenseCalculator {
@@ -123,15 +126,61 @@ public class ExpenseCalculator implements IExpenseCalculator {
     }
 
     @Override
-    public boolean loadExpenseFile(String filePath) {
-        return false;
+public boolean loadExpenseFile(String ExReport) {
+    	
+    	String ExpenseReport = "output/UserExpenseReport.txt";
+        if (userAtHand == null)
+            return false;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(ExpenseReport))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                if (parts.length == 3) {
+                    String source = parts[0];
+                    double amount = Double.parseDouble(parts[1]);
+                    int yearlyFrequency = Integer.parseInt(parts[2]);
+                    Expense expense = new Expense(source, amount, yearlyFrequency);
+                    userAtHand.addExpense(expense);
+                } else {
+                    
+                    return false;
+                }
+            }
+            return true;
+        } catch (IOException | NumberFormatException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     @Override
-    public boolean loadIncomeFile(String filePath) {
-        return false;
-    }
+    public boolean loadIncomeFile(String InReport) {
+    	String IncomeReport = "output/UserIncomeReport.txt";
+    	 if (userAtHand == null)
+             return false;
 
+         try (BufferedReader reader = new BufferedReader(new FileReader(IncomeReport))) {
+             String line;
+             while ((line = reader.readLine()) != null) {
+                 String[] parts = line.split(",");
+                 if (parts.length == 3) {
+                     String source = parts[0];
+                     double amount = Double.parseDouble(parts[1]);
+                     String month = parts[2];
+                     Wage wage = new Wage(source, amount, month);
+                     userAtHand.addIncome(wage);
+                 } else {
+                     
+                     return false;
+                 }
+             }
+             return true;
+         } catch (IOException | NumberFormatException e) {
+             e.printStackTrace();
+             return false;
+         }
+    }
     @Override
     public int whenCanIBuy(String itemName, double price) {
         return 0;
