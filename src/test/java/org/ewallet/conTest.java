fa66dc1868ed68b28ae4f2686261d6a8a8e0ae4d
Conner <conneryule@gmail.com>
2024-08-06 13:51:13 -0400

    houseKeeping

---
+++ src/test/java/org/ewallet/conTest.java
@@ -0,0 +1,16 @@
+package org.ewallet;
+
+import org.junit.*;
+import org.ewallet.*;
+
+public class conTest {
+
+	@Test
+	public void test() {
+
+		connection con = new connection();
+		con.getConnection();
+
+	}
+
+}
