4e0ae3b20c4911a2a5444406f558c95d4eef2ab4
Conner <conneryule@gmail.com>
2024-08-06 23:26:48 -0400

    housekeeping

--- src/main/java/org/ewallet/GUI.java
+++ src/main/java/org/ewallet/GUI.java
@@ -2,8 +2,6 @@ package org.ewallet;
 
 import java.awt.EventQueue;
 import java.awt.Font;
-import java.io.*;
-import java.awt.Label;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
